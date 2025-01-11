900aa67bc6e4b0cdd312ecfc68d33dbcb0d6ea18
yawarpwn <john.kazz@gmail.com>
2025-01-10 23:45:28 -0500

    add todo scheme

--- src/db/schemas/index.ts
+++ src/db/schemas/index.ts
@@ -1,7 +1,15 @@
+import { sql } from "drizzle-orm";
 import { int, sqliteTable, text } from "drizzle-orm/sqlite-core";
 
 export const usersTable = sqliteTable("users", {
   id: int().primaryKey({ autoIncrement: true }),
   email: text().notNull().unique(),
   password: text().notNull(),
+});
+
+export const todoTable = sqliteTable("todo", {
+  id: int({ mode: "number" }).primaryKey({ autoIncrement: true }),
+  complete: int({ mode: "boolean" }).default(false),
+  text: text().notNull(),
+  createAt: text().default(sql`(CURRENT_DATE)`),
 });
--- src/db/schemas/users.ts
+++
@@ -1,7 +0,0 @@
-import { int, sqliteTable, text } from "drizzle-orm/sqlite-core";
-
-export const usersTable = sqliteTable("users", {
-  id: int().primaryKey({ autoIncrement: true }),
-  email: text().notNull().unique(),
-  password: text().notNull(),
-});
