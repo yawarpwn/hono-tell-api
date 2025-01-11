34aaf1c80e01b5c73c22599a60180361fc9b050e
yawarpwn <john.kazz@gmail.com>
2025-01-10 23:09:04 -0500

    add routes _

--- src/index.ts
+++ src/index.ts
@@ -3,18 +3,18 @@ import { jwt } from "hono/jwt";
 import { cors } from "hono/cors";
 import { prettyJSON } from "hono/pretty-json";
 import { drizzle } from "drizzle-orm/d1";
-import * as schema from "./db/schemas";
+import * as schemas from "./db/schemas";
 import type { App } from "./types";
 import { basicAuth } from "hono/basic-auth";
 
 const app = new Hono<App>();
 
-app.use("*", cors());
-app.use("*", prettyJSON());
+// app.use("*", cors());
+// app.use("*", prettyJSON());
 
 //DatabaseMiddleware
 app.use("/api/*", async (c, next) => {
-  const db = drizzle(c.env.DB_TELL, { schema });
+  const db = drizzle(c.env.DB_TELL, { schema: schemas });
   c.set("db", db);
   await next();
 });
@@ -27,6 +27,10 @@ app.use("/api/*", async (c, next) => {
 
 app.get("/api/auth", async (c) => {
   console.log("in /api/auth success");
+  const db = c.get("db");
+  const result = await db.select().from(schemas.usersTable).all();
+  console.log(result);
+
   return c.text("sucess");
 });
 
